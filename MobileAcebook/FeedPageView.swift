790b815c2c020055fdcf889e1378c26359265ef8
Ani <anisingh@outlook.com>
2023-10-12 11:03:00 +0100

    Fix background colour

--- MobileAcebook/FeedPageView.swift
+++ MobileAcebook/FeedPageView.swift
@@ -12,48 +12,54 @@ struct FeedPageView: View {
     @State var posts = [Post]()
     
     var body: some View {
-        
-        HStack {
-            Text("Username")
-            Spacer()
-            Image("makers-logo")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 80, height: 80)
-                .accessibilityIdentifier("makers-logo")
-            //                .padding(.top, 30)
-            Spacer()
-            Button(action: {
-                // add line to new post
-            },label: {
-                Text("Post")
-                    .foregroundColor(Color.white)
-                    .font(.headline)
-            }
-            ).padding(.vertical, 15)
-                .padding(.horizontal, 30)
-                .background(Color("CTA"))
-                .foregroundColor(.white)
-                .cornerRadius(10)
-        }
-        
-        List(posts, id: \._id) { post in
-            Text(post.message)
-        }
-        .onAppear {
-            guard var token = UserDefaults.standard.string(forKey: "token") else {
-                return
-            }
-            service.loadPosts(token: token) { (posts, err) in
-                guard var posts = posts else {
-                    // handle error
-                    return
+        ZStack {
+            AcebookBg()
+            VStack {
+                HStack {
+                    Spacer()
+                    Text("Username")
+                    Spacer()
+                    Image("makers-logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 80, height: 80)
+                        .accessibilityIdentifier("makers-logo")
+                    //                .padding(.top, 30)
+                    Spacer()
+                    NavigationLink {
+                        NewPostView()
+                    } label: {
+                        Text("Post")
+                            .frame(width: 60, height: 60)
+                            .background(Color("CTA"))
+                            .foregroundColor(.white)
+                            .clipShape(Circle())
+                    }
+                    Spacer()
                 }
-                self.posts = posts.posts
+                
+                VStack {
+                ForEach(posts, id: \._id) { post in
+                        Text(post.message)
+                    }
+                }
+                .onAppear {
+                    guard var token = UserDefaults.standard.string(forKey: "token") else {
+                        return
+                    }
+                    service.loadPosts(token: token) { (posts, err) in
+                        guard var posts = posts else {
+                            // handle error
+                            return
+                        }
+                        self.posts = posts.posts
+                    }
+                }
             }
         }
     }
 }
+
 
 
 
